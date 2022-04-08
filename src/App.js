ad2539e8271962762f3e7cb125590f4cd49154ad
Davood Khoshnood <45537769+DavoodKhoshnood@users.noreply.github.com>
2022-04-08 03:20:06 +0100

    new Changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import countriesAll from './countriesAll.json'
 import './App.css'
 
 function App() {
+  const regions = [...new Set(countriesAll.map((country) => country.region))]
   const [countries, setCountries] = useState(countriesAll)
+  // const [selected, setSelected] = useState(null)
+  
+  
   const filter = (inputValue) => {
+    document.getElementById('select').value = 'All countries' 
     setCountries(
       countriesAll.filter((country) =>
         country.name
@@ -15,23 +20,46 @@ function App() {
       ),
     )
   }
+
+  const changeSelect = (selected) => {
+    document.getElementById('input').value = ''
+    setCountries(
+      selected === 'All countries'?countriesAll:countriesAll.filter((country) =>
+        country.region
+          .toLocaleLowerCase()
+          .includes(selected.toLocaleLowerCase()),
+      ),
+    )
+  }
   return (
     <>
       <div className="App">
         <header className="App-header">Where in the world?</header>
       </div>
-      <div className="m-5">
+      <div className="form-row m-5">
         <input
-          className="form-control"
+          id='input'
+          className="form-control col-md-9 m-2"
           type="search"
           placeholder="Search..."
           onChange={(e) => filter(e.target.value)}
         />
-        <div className="card-deck p-1">
-          {countries.map((country, index) => (
-            <Card data={country} index = {index} />
+
+        <select
+        id='select'
+          onChange={(e) => changeSelect(e.target.value)}
+          className="form-control col-md-2 m-2"
+        >
+          <option>All countries</option>
+          {regions.map((region) => (
+            <option>{region}</option>
           ))}
-        </div>
+        </select>
+      </div>
+      <div className="card-deck p-1">
+        {countries.map((country, index) => (
+          <Card data={country} index={index} />
+        ))}
       </div>
     </>
   )
